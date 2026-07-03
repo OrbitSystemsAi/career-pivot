277b6e184ec0aab310ca4a75bd8e91e45576a4db
Earl Powery <earlpowery@Mac.attlocal.net>
2026-07-03 15:18:38 -0400

    Add career module public API and active module state

---
+++ core/state/activeModule.ts
@@ -0,0 +1,5 @@
+export const activeModule = {
+  id: "career",
+  name: "Career",
+  view: "visualization",
+};
---
+++ modules/career/index.ts
@@ -0,0 +1,7 @@
+export { default as CareerGraph } from "./components/CareerGraph";
+
+export { default as CareerStrengths } from "./components/CareerStrengths";
+
+export { default as CareerSuggestions } from "./components/CareerSuggestions";
+
+export { default as CareerWords } from "./components/CareerWords";
