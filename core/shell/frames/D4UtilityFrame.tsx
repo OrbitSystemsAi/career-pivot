e6928acf5a5337544a9e98cecbfc8cb548d8e799
Earl Powery <earlpowery@Mac.attlocal.net>
2026-07-03 16:38:12 -0400

    Load utility panels from active module

--- core/shell/frames/D4UtilityFrame.tsx
+++ core/shell/frames/D4UtilityFrame.tsx
@@ -1,26 +1,34 @@
-import D4UtilityTopPanel from "../panels/D4UtilityTopPanel";
-import D4UtilityMiddlePanel from "../panels/D4UtilityMiddlePanel";
-import D4UtilityBottomPanel from "../panels/D4UtilityBottomPanel";
+"use client";
+
+import { getActiveModule } from "@/core/modules/getActiveModule";
+import { useActiveModule } from "@/core/state/ActiveModuleProvider";
 
 type Props = {
   section: "top" | "bottom";
 };
 
 export default function D4UtilityFrame({ section }: Props) {
+  const { activeModule } = useActiveModule();
+  const module = getActiveModule(activeModule);
+
+  const UtilityTop = module.panels.utilityTop;
+  const UtilityMiddle = module.panels.utilityMiddle;
+  const UtilityBottom = module.panels.utilityBottom;
+
   if (section === "bottom") {
     return (
       <aside className="h-full min-h-0">
-        <D4UtilityBottomPanel />
+        <UtilityBottom />
       </aside>
     );
   }
 
   return (
     <aside className="flex h-full min-h-0 flex-col gap-4">
-      <D4UtilityTopPanel />
+      <UtilityTop />
 
       <div className="min-h-0 flex-1">
-        <D4UtilityMiddlePanel />
+        <UtilityMiddle />
       </div>
     </aside>
   );
