024757b48e2bc6ff7e5617843f08e0ebd6730b38
Earl Powery <earlpowery@Mac.attlocal.net>
2026-07-07 05:45:25 -0400

    Connect D5 resume actions to resume engine

--- modules/resume/components/ResumeActions.tsx
+++ modules/resume/components/ResumeActions.tsx
@@ -1,13 +1,52 @@
+"use client";
+
 import ActionRow from "@/core/ui/ActionRow";
 import PanelCard from "@/core/ui/PanelCard";
-import { resumeActions } from "../data/resumeData";
+import { useOSState } from "@/core/state/OSStateProvider";
+import { useUser } from "@/core/user/UserProvider";
 
 export default function ResumeActions() {
+  const { setActiveView } = useOSState();
+  const { user, activeResumeId, optimizeResume } = useUser();
+
+  const activeResume =
+    user.resumes.find((resume) => resume.id === activeResumeId) ??
+    user.resumes[0];
+
+  function handleOptimize() {
+    if (!activeResume) {
+      return;
+    }
+
+    optimizeResume(activeResume.id, "full_rewrite");
+    setActiveView("versions");
+  }
+
   return (
     <PanelCard title="Actions">
-      {resumeActions.map((item) => (
-        <ActionRow key={item} label={item} action="Run" />
-      ))}
+      <ActionRow
+        label="Optimize Resume"
+        action="Run"
+        onClick={handleOptimize}
+      />
+
+      <ActionRow
+        label="Review Document"
+        action="Open"
+        onClick={() => setActiveView("document")}
+      />
+
+      <ActionRow
+        label="Compare Versions"
+        action="Open"
+        onClick={() => setActiveView("compare")}
+      />
+
+      <ActionRow
+        label="Request Peer Review"
+        action="Request"
+        onClick={() => setActiveView("summary")}
+      />
     </PanelCard>
   );
 }
